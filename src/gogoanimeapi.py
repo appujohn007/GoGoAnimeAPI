32a9601088683038c7500e6294c71daff00357dc
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-28 18:38:00 +0530

    Update gogoanimeapi.py

--- src/gogoanimeapi.py
+++ src/gogoanimeapi.py
@@ -33,44 +33,66 @@ class gogoanime():
             return {"status":"404", "reason":"Check the host's network Connection"}
 
     def get_anime_details(animeid):
-        try:
-            animelink = 'https://anitaku.to/category/{}'.format(animeid)
-            response = requests.get(animelink)
-            plainText = response.text
-            soup = BeautifulSoup(plainText, "lxml")
-            source_url = soup.find("div", {"class": "anime_info_body_bg"}).img
-            imgg = source_url.get('src')
-            tit_url = soup.find("div", {"class": "anime_info_body_bg"}).h1.string
-            lis = soup.find_all('p', {"class": "type"})
-            print(f"list = {lis}")
-            plot_sum = lis[1]
-            pl = plot_sum.get_text().split(':')
-            pl.remove(pl[0])
-            sum = ""
-            plot_summary = sum.join(pl)
-            type_of_show = lis[0].a['title']
-            ai = lis[2].find_all('a')  # .find_all('title')
-            genres = []
-            for link in ai:
-                genres.append(link.get('title'))
-            year1 = lis[3].get_text()
-            year2 = year1.split(" ")
-            year = year2[1]
-            status = lis[4].a.get_text()
+    try:
+        animelink = 'https://anitaku.to/category/{}'.format(animeid)
+        response = requests.get(animelink)
+        plainText = response.text
+        soup = BeautifulSoup(plainText, "lxml")
+        source_url = soup.find("div", {"class": "anime_info_body_bg"}).img
+        imgg = source_url.get('src')
+        tit_url = soup.find("div", {"class": "anime_info_body_bg"}).h1.string
+        lis = soup.find_all('p', {"class": "type"})
+        print(f"list = {lis}")
+        
+        plot_sum = lis[1]
+        pl = plot_sum.get_text().split(':')
+        pl.remove(pl[0])
+        sum = ""
+        plot_summary = sum.join(pl)
+        
+        type_of_show = lis[0].a['title']
+        ai = lis[2].find_all('a')
+        genres = [link.get('title') for link in ai]
+        
+        year1 = lis[3].get_text()
+        year2 = year1.split(" ")
+        year = year2[1]
+        
+        status = lis[4].a.get_text()
+        
+        # Check if the list has at least 6 elements
+        if len(lis) > 5:
             oth_names = lis[5].get_text()
-            lnk = soup.find(id="episode_page")
-            ep_str = str(lnk.contents[-2])
-            a_tag = ep_str.split("\n")[-2]
-            a_tag_sliced = a_tag[:-4].split(">")
-            last_ep_range = a_tag_sliced[-1]
-            y = last_ep_range.split("-")
-            ep_num = y[-1]
-            res_detail_search = {"title":f"{tit_url}", "year":f"{year}", "other_names":f"{oth_names}", "type":f"{type_of_show}", "status":f"{status}", "genre":f"{genres}", "episodes":f"{ep_num}", "image_url":f"{imgg}","plot_summary":f"{plot_summary}"}
-            return res_detail_search
-        except AttributeError:
-            return {"status":"400", "reason":"Invalid animeid"}
-        except requests.exceptions.ConnectionError:
-            return {"status":"404", "reason":"Check the host's network Connection"}
+        else:
+            oth_names = None
+        
+        lnk = soup.find(id="episode_page")
+        ep_str = str(lnk.contents[-2])
+        a_tag = ep_str.split("\n")[-2]
+        a_tag_sliced = a_tag[:-4].split(">")
+        last_ep_range = a_tag_sliced[-1]
+        y = last_ep_range.split("-")
+        ep_num = y[-1]
+        
+        res_detail_search = {
+            "title": tit_url,
+            "year": year,
+            "other_names": oth_names,
+            "type": type_of_show,
+            "status": status,
+            "genre": genres,
+            "episodes": ep_num,
+            "image_url": imgg,
+            "plot_summary": plot_summary
+        }
+        
+        return res_detail_search
+    
+    except AttributeError:
+        return {"status": "400", "reason": "Invalid animeid"}
+    
+    except requests.exceptions.ConnectionError:
+        return {"status": "404", "reason": "Check the host's network Connection"}
 
     def get_episodes_link(animeid, episode_num):
         try:
